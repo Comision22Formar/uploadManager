c5511cb54fe33aaca9132b98bf498f3699b7f132
Eric <menaericdaniel@gmail.com>
2023-12-19 10:37:56 -0300

    feat: add product one-image done!

---
+++ controllers/indexController.js
@@ -0,0 +1,10 @@
+const { readJSON } = require("../data")
+
+module.exports = {
+    index : (req,res) => {
+        const productsOneImage = readJSON('productsOneImage.json')
+        return res.render('index',{
+            productsOneImage
+        })
+    }
+}
---
+++ controllers/productMultipleImagesController.js
@@ -0,0 +1,21 @@
+module.exports = {
+    list : (req,res) => {
+
+    },
+    add : (req,res) => {
+        return res.render('productAddMultipleImages')
+    },
+    create :  (req,res) => {
+
+    },
+    edit : (req,res) => {
+
+    },
+    update : (req,res) => {
+
+    },
+    remove : (req,res) => {
+        
+    }
+
+}
